7a99f6e8d8ea6c1cf6a75a0373fdff34425b16f6
Alibek <alibek@192.168.88.114>
2021-07-20 20:18:34 +0600

    algorithms

--- algorithm_tasks.py
+++ algorithm_tasks.py
@@ -7,6 +7,18 @@ a, b = b, a
 
 
 #2 reverse time
+h = int(input("Введите часы: "))
+m = int(input("Введите минуты: "))
 
-
-
+if h > 12 or h < 0:
+	print("Неправильный час")
+elif m > 60 or m < 0:
+	print("Неправильная минута")
+else:
+	x = 12 - h
+	y = 60 - m
+	if x == 12:
+		x = 0
+	elif y == 60:
+		y = 0
+	print(x, y)

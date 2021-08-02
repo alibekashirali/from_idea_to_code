a1d587ab95cba1cd52e43cebe97f101a020165bb
Alibek <alibek@192.168.88.218>
2021-08-03 00:31:23 +0600

    algorithms

--- algorithm_tasks.py
+++ algorithm_tasks.py
@@ -22,3 +22,31 @@ else:
 	elif y == 60:
 		y = 0
 	print(x, y)
+
+
+#3 checkmate
+a = [int(x) for x in input("Введите числа через пробел: ").split()]
+
+if len(a) < 4:
+	print("Вы ввели недостаточно данных")
+elif max(a) > 8:
+	print("Вы ввели значение больше 8")
+else:
+	if a[0] == a[2] or a[0] == a[2]:
+		print("YES")
+	elif a[1] == a[2] or a[1] == a[3]:
+		print("YES")
+	else:
+		print("NO")
+
+
+#4 max number
+nums = [x for x in input("Введите числа через пробел: ").split()]
+
+a, b = nums[0] + nums[1] + nums[2], nums[1] + nums[0] + nums[2]
+c, d = nums[0] + nums[2] + nums[1], nums[1] + nums[2] + nums[0]
+e, f = nums[2] + nums[1] + nums[0], nums[2] + nums[0] + nums[1]
+
+array = [a, b, c, d, e, f]
+
+print(max(array))
